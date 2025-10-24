c008c0d599ff8445c0844de0c1b8d4699dd20b1c
brumble9401 <tranhungcuong0904@gmail.com>
2025-10-24 14:12:45 +0700

    Add Burn event to contract to improve burn operation visibility

--- contracts/Faster.sol
+++ contracts/Faster.sol
@@ -5,6 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract Faster is ERC20, ERC20Permit {
+    // Custom event for burn operations
+    event Burn(address indexed account, uint256 amount);
+
     constructor(
         address treasury
     ) ERC20("Archer Hunter", "FASTER") ERC20Permit("Archer Hunter") {
@@ -14,5 +17,6 @@ contract Faster is ERC20, ERC20Permit {
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+        emit Burn(msg.sender, amount);
     }
 }
